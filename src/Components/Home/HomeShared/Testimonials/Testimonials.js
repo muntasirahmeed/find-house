bd78bff846fdca9b3934ca3b41b15cb9b8ba4ef9
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-07-23 14:51:49 +0600

    testimonial done

---
+++ src/Components/Home/HomeShared/Testimonials/Testimonials.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../../../styles/css/swiper.css";
+import "../../../../styles/css/style.css";
+import { Pagination } from "swiper";
+const Testimonials = () => {
+  const data = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/McBj6Gd/2.jpg",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/wCmBT7r/2.png",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/v1ndN0y/4.png",
+    },
+    {
+      id: 4,
+      img: "https://i.ibb.co/7z0FMmX/6.png",
+    },
+    {
+      id: 5,
+      img: "https://i.ibb.co/VgjYfpS/7.png",
+    },
+  ];
+  return (
+    // <!-- Our Testimonials -->
+    <section id="our-testimonials" class="our-testimonial">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 offset-lg-3">
+            <div class="main-title text-center">
+              <h2 class="color-white">Testimonials</h2>
+              <p class="color-white">Here could be a nice sub title</p>
+            </div>
+          </div>
+        </div>
+        <Swiper
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          speed={1100}
+          className=""
+        >
+          <div class="row">
+            <div class="col-lg-6 offset-lg-3">
+              <div class="testimonial_grid_slider">
+                {data.map((img) => (
+                  <SwiperSlide>
+                    <div class="item">
+                      <div class="testimonial_grid">
+                        <div class="thumb">
+                          <img src={img.img} alt="1.jpg" />
+                        </div>
+                        <div class="details">
+                          <h4>Augusta Silva</h4>
+                          <p>Sales Manager</p>
+                          <p class="mt25">
+                            Aliquam dictum elit vitae mauris facilisis at dictum
+                            urna dignissim donec vel <br /> lectus vel felis.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </div>
+            </div>
+          </div>
+        </Swiper>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
--- src/Components/Home/Page-1/HomePage1.js
+++ src/Components/Home/Page-1/HomePage1.js
@@ -3,6 +3,7 @@ import Footer from "../../Shared/Footer/Footer";
 import BecomeAgent from "../HomeShared/BecomeAgent";
 import OurPartners from "../HomeShared/OurPartners";
 import PropertyCities from "../HomeShared/PropertyCities";
+import Testimonials from "../HomeShared/Testimonials/Testimonials";
 import WhyChoseUs from "../HomeShared/WhyChoseUs";
 
 const Page_1 = () => {
@@ -10,6 +11,7 @@ const Page_1 = () => {
     <>
       <PropertyCities />
       <WhyChoseUs />
+      <Testimonials />
       <OurPartners />
       <BecomeAgent />
       <Footer />
